defb33c7e0b225b2a05cebca1cefafb80662c172
CB Averitt <cb.averitt@deque.com>
2017-11-16 15:39:01 -0700

    lots of changes

--- presentation-mechanics.js
+++ presentation-mechanics.js
@@ -2,7 +2,7 @@ $(function() {
 	$('body').append('<div id="dialog" role="alertdialog" tabindex="-1" aria-labelledby="dialog-head" aria-describedby="dialog-body" /><button type="button" id="dialog-trigger">Results</button>');
 	$('#dialog').load('dialog.html', function() {
     var url = window.location.pathname;
-		$('#result-row').load(url.substring(url.lastIndexOf('/') + 1, url.length - 5) + '-result.html');
+		$('tbody').load(url.substring(url.lastIndexOf('/') + 1, url.length - 5) + '-result.html');
 		$('#dialog-trigger').on('click', function (e) {
 			e.stopPropagation();
 			$('#dialog').fadeIn('fast');
